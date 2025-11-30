9f54a971cc0596f23a5b6231710fa2d952e19c84
Marc Prewitt <mdprewitt@users.noreply.github.com>
2025-11-29 21:06:12 -0500

    Add Strudel file for 'Steppin' Out' by Joe Jackson

---
+++ steppin_out.js
@@ -0,0 +1,5 @@
+// Strudel File
+// Steppin' out, Joe Jackson
+bd_snare: s("[bd!2, ~ sd]*2").bank("bossdr110").gain(.4)
+hh: s("hh!8").bank("ace").hpf(10000).gain(.4)
+bass: note("d2!2 a1!2 g1!2 a1!2 ").sound("gm_fretless_bass").gain(1.2)
