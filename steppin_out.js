317a386e0c23caa133002d2d9a6251b4400eb11a
Marc Prewitt <mdprewitt@users.noreply.github.com>
2025-12-14 15:10:03 -0500

    d is doubled in first measure of cycle

--- steppin_out.js
+++ steppin_out.js
@@ -1,5 +1,7 @@
 // Strudel File
 // Steppin' out, Joe Jackson
-bd_snare: s("[bd!2, ~ sd]*2").bank("bossdr110").gain(.4)
+setcpm(20)
+bd_snare: s("[bd!2, ~ sd]*4").bank("bossdr110").gain(.4)
 hh: s("hh!8").bank("ace").hpf(10000).gain(.4)
-bass: note("d2!2 a1!2 g1!2 a1!2 ").sound("gm_fretless_bass").gain(1.2)
+bass: note("d2!4 g1!2 a1!2 d2!2 a1!2 g1!2 a1!2 ").velocity(.7).sound("gm_fretless_bass").gain(1.2)
+
